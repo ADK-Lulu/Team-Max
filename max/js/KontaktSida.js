073fa79ea780cce8eeb44d097912d5edaad10656
Paatsu <johnny@wong.se>
2020-03-04 18:15:38 +0100

    Task 14.1 Uppdaterat KontaktSida med ett formulär

--- max/js/KontaktSida.js
+++ max/js/KontaktSida.js
@@ -3,10 +3,31 @@ class KontaktSida extends Base {
   render() {
     return /*html*/`
       <div class="container">
-        <div class="row" route="/kontakt-sida" page-title="Kontakta oss">
-          <div class="col-12">
-            <h1>Kontakta oss</h1>
-            <p>Det här är en sida där du kan kontakta oss</p>
+        <div route="/kontakt-sida" page-title="Kontakta oss">
+          <div class="row">
+                <div class="col-3">
+                  <h1>Kontakta oss:</h1>
+                </div>
+                <div class="col-9">
+                  <form submit="collectFormData">
+                    <div class="form-group">
+                      <label class="w-100">Namn:
+                        <input name="namn" type="text" class="form-control" placeholder="Ditt namn" required pattern=".{2,}">
+                      </label>
+                    </div>
+                    <div class="form-group">
+                      <label class="w-100">E-post:
+                        <input name="epost" type="email" class="form-control" placeholder="Din e-postadress" required>
+                      </label>
+                    </div>
+                    <div class="form-group">
+                      <label class="w-100">Meddelande:
+                        <textarea name="fritext" class="form-control" placeholder="Skriv ditt meddelande här" required></textarea>
+                      </label>
+                    </div>
+                    <input class="btn btn-primary float-right" type="submit" value="Skicka">
+                  </form>
+              </div> 
           </div>
         </div>
       </div>
