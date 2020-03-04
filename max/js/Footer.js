984efee2a9c8781cc3aaa81ee7f3c88496420271
teknikMarit <teknikMarit@gmail.com>
2020-03-04 10:06:37 +0100

    Task 3.2 knapp+länk från fotter till kontaktsida

--- max/js/Footer.js
+++ max/js/Footer.js
@@ -5,7 +5,8 @@ class Footer extends Base {
       <footer class="bg-primary p-4 container-fluid">
         <div class="row">
           <div class="col text-center text-light">
-            © Dhyr & Rumson 2020
+          <a href="/kontakt-sida" class="btn btn-info" role="button">Mer om oss</a>
+          <p>© Dhyr & Rumson 2020</p>
           </div>
         </div>
       </footer>
