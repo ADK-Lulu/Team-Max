39e5576ece9722e715eea9298ddeea1a722a1a36
ADK-Lulu <ulrika.alehag@gmail.com>
2020-03-05 14:36:41 +0100

    Fixade/städade task 13.1

--- max/js/KontaktSida.js
+++ max/js/KontaktSida.js
@@ -27,23 +27,21 @@ class KontaktSida extends Base {
 
   render() {
     return /*html*/`
-    <div route="/kontakt-sida" page-title="Kontakta oss">
-      <div class="container">
-        <div class="row mx-md-n5">
-          <div class="col px-md-5">
-            <div class="p-3 border bg-light"><img src="images/misc/kontaktsida_kontorsfasad.jpg" class="card-img-top" alt="">
-          </div>
-          </div>
+      <div route="/kontakt-sida" page-title="Kontakta oss">
+        <div class="container">
+          <div class="row mx-md-n5">
+            <div class="col px-md-5">
+              <div class="p-3 border bg-light"><img src="images/misc/kontaktsida_kontorsfasad.jpg" class="card-img-top" alt="">
+              </div>
+            </div>
             <div class="col px-md-5">
               <div class="p-3 border bg-light">
-              <h2>Dhyr & Rumson</h2><br>Adress: Liljeholmsgatan 123<br>Liljeholmen<br>Öppettider<br>Måndag-Fredag: 10-16<br>Lördag-Söndag: Stängt
-            <br>Telefon: +46 23 456 78 90<br>Email:<a href="mailto:dhyrochrumson@email.com">dhyrochrumson@email.com</a>
-             </div>
-             </div>
-     </div>
-      </div>
-        
-         <div class="row">
+                <h2>Dhyr & Rumson</h2><br>Adress: Liljeholmsgatan 123, Liljeholmen<br>Öppettider:<br>Måndag-Fredag: 10-16<br>Lördag-Söndag: Stängt
+                <br>Telefon: +46 23 456 78 90<br>Email:<a href="mailto:dhyrochrumson@email.com">dhyrochrumson@email.com</a>
+              </div>
+            </div>
+          </div>
+          <div class="row">
             ${this.formSent ? /*html*/`
               <div class="col-12">
                 <h2>
@@ -51,17 +49,15 @@ class KontaktSida extends Base {
                   <p>Vi återkommer till dig så snabbt som möjligt.</p>
                 </h2>
               </div>
-                  ` :
-                  /*html*/`
-
-                
-          <div class="col px-md-4 mt-5">
-            <div class="float-right">
-                <h4>Kontakta oss:</h4>
-            </div>
+              `:
+                /*html*/`  
+              <div class="col px-md-4 mt-5">
+                <div class="float-right">
+                  <h4>Kontakta oss:</h4>
+                </div>
               </div>
-                <div class="col-8 mt-5">
-                  <form submit="collectFormData">
+              <div class="col-8 mt-5">
+                <form submit="collectFormData">
                     <div class="form-group">
                       <label class="w-100">Namn:
                         <input name="namn" type="text" class="form-control" placeholder="Ditt namn" required pattern=".{2,}">
