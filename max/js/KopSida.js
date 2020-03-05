66671c73a3f0b8f785da77cda6b2e1bc4f1e7f5c
teknikMarit <teknikMarit@gmail.com>
2020-03-05 15:15:25 +0100

    Task 5.1 Ordnat bostadstyp-checkboxes

--- max/js/KopSida.js
+++ max/js/KopSida.js
@@ -7,6 +7,30 @@ class KopSida extends Base {
           <div class="col-12">
             <h1>Köpa bostad</h1>
             <p>Det här är en sida där du kan köpa bostad</p>
+
+    
+            	<form>
+              <div class="form-row align-items-center">
+                  <div class="col-md-3"><label class="btn btn-secondary">
+                  <img src="/images/iconer/bostadsratt.png" alt="bostadsratt">
+                  <input type="checkbox"  id="bostadsratt"></label></div>
+
+                  <div class="col-md-3"><label class="btn btn-secondary">
+                  <img src="/images/iconer/radhus.png" alt="radhus">
+                  <input type="checkbox" id="radhus" ></label></div>
+
+                  <div class="col-md-3"><label class="btn btn-secondary">
+                  <img src="/images/iconer/hus.png" alt="hus">
+                  <input type="checkbox" id="hus"></label></div>
+              
+
+                  <div class="col-md-3"><label class="btn btn-secondary">
+                  <img src="/images/iconer/nybygge.png" alt="nybygge">
+                  <input type="checkbox" id="nybygge"></label></div>
+              </div>
+                
+                </form>
+                        
           </div>
         </div>
       </div>
