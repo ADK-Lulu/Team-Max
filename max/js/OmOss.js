2af0d9204ae8689035d9978ac6870b3d7da03eff
jflo846 <floden.jennie@gmail.com>
2020-03-09 18:22:47 +0100

    task 15.1 formulär för att sälja.

--- max/js/OmOss.js
+++ max/js/OmOss.js
@@ -4,8 +4,41 @@ class OmOss extends Base {
     return /*html*/`
       <div class="row" route="/om-oss" page-title="Om oss">
         <div class="col-12">
-          <h1>Om oss</h1>
-          <p>Det här är en sida om oss.</p>
+          <h1>Vi är Dhyr & Rumson</h1>
+           <h3>Redo att sälja? Fyll i formuläret så kontaktar vi dig!</h3>
+           <form>
+            <div class="form-row">
+              <div class="form-group col-md-4">
+               <label>För- och efternamn:</label>
+                <input type="text" class="form-control">
+              </div>
+              <div class="form-group col-md-4">
+               <label>Område:</label>
+                <input type="text" class="form-control">
+              </div>
+            </div>
+            <div class="form-row">
+              <div class="form-group col-md-4">
+               <label>Email:</label>
+                <input type="email" class="form-control">
+              </div>
+              <div class="form-group col-md-4">
+               <label>Kvm:</label>
+                <input type="text" class="form-control">
+              </div>
+            </div>
+            <div class="form-row">
+              <div class="form-group col-md-4">
+               <label>Telefonnummer:</label>
+                <input type="text" class="form-control">
+              </div>
+              <div class="form-group col-md-4">
+               <label>Antal rum:</label>
+                <input type="text" class="form-control">
+              </div>
+            </div>
+            <button type="submit" class="btn btn-primary">Skicka</button>
+           </form>
         </div>
       </div>
     `;
