5953cf295a771150582f9dadee7c04c31a55bc2d
teknikMarit <teknikMarit@gmail.com>
2020-03-09 20:24:52 +0100

    16.1 Info om företaget+motto

--- max/js/OmOss.js
+++ max/js/OmOss.js
@@ -2,10 +2,24 @@ class OmOss extends Base {
 
   render() {
     return /*html*/`
-      <div class="row" route="/om-oss" page-title="Om oss">
-        
-      </div>
-    `;
+<div class="container" route="/om-oss" page-title="Om oss">
+  <div class="row">
+    <!--info om företaget-->
+    <div class="col-4">
+      <H1>Vår story</H1>
+      <p>Redan när vår grundare <strong>Fiona</strong> var en liten flicka så älskade hon hus. Hon ritade hus.
+        Hon byggde hus av sockerbitar. Hon byggde hus av lego.
+        Hon byggde pepparkakshus som hon sedan sprängde med ettöressmällare.
+        Och nu får hon jobba med det hon älskar mest:
+        Att ta dina pengar för att köpa hus. Eller med hennes egna ord:</p>
+      <H4><i>"Dina besparingar - mitt semesterparadis på Rhodos."</i></H4>
+    </div>
+    <!--info + bildfrån db om mäklarna-->
+    <div class="col-8">
+    </div>
+  </div>
+</div>
+`;
   }
 
 }
