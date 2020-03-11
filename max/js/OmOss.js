4fdeaf97f2e9d3ebebdc2ff28b53643b14a3e525
teknikMarit <teknikMarit@gmail.com>
2020-03-11 13:20:14 +0100

    Gjort mäklarsida responsiv o lagt till kolumn i db

--- max/js/OmOss.js
+++ max/js/OmOss.js
@@ -11,19 +11,19 @@ class OmOss extends Base {
     bildUrl,
     namn,
     telefonnummer,
-    epost
+    epost,
+    personligt
     FROM Maklare`)
   }
 
 
   render() {
     return /*html*/`
-<div class="container fluid" route="/om-oss" page-title="Om oss">
-  <div class="row">
-
+<div class="row" route="/om-oss" page-title="Om oss">
+  
     <!--infofält om företaget o chefen-->
-    <div class="col-5 bg-secondary">
-      <div class="card my-4">
+    <div class="col-md-6 col-xl-6 bg-secondary">
+      <div class="card col-my-2">
         <div class="card-body">
           <h4 class="card-title">Vår story</h4>
           <p class="card-text">Redan när vår grundare <strong>Fiona</strong> var en liten flicka så älskade hon hus. Hon
@@ -31,28 +31,29 @@ class OmOss extends Base {
             Hon byggde hus av sockerbitar. Hon byggde hus av lego. Hon byggde pepparkakshus som hon sedan sprängde med
             smällare.
             Och nu får hon jobba med det hon älskar mest: Att ta dina pengar för att köpa hus. Eller med hennes egna
-            ord: </p>
-          <p class="card-text"><small class="text-muted">
-              <H4><i>"Dina besparingar - mitt semesterparadis på Rhodos."</i></H4>
-            </small></p>
+            ord: 
+          </p>
+            <p class="card-text"><small class="text-muted">
+              <H4><i>"Dina besparingar - mitt semesterparadis på Rhodos."</i></H4></small>
+            </p>
           <img class="card-img-bottom" src="/images/maklare/2.jpg" alt="Företagschefen">
         </div>
       </div>
     </div>
 
     <!--info om samt bilder från db om mäklarna.-->
-    <div class="col-7">
-      <div class="card-columns">
+    <div class="col-md-6 col-xl-6">
+      <div class="row card-columns">
         ${this.maklarInfo.map(info =>/*html*/`
-        <div class="card mb-4">
+        <div class="col-md-6 card-mb-2">
           <img class="card-img-top" src="${info.bildUrl}" alt="Card image cap">
           <div class="card-body">
             <h5 class="card-title">${info.namn}</h5>
-            <ul class="list-unstyled">
-              <li>${info.telefonnummer}</li>
-              <li>${info.epost}</li>
-            </ul>
-            <p class="card-text"><small class="text-muted">Tramstext om vad de gillar för bostäder</small></p>
+              <ul class="list-unstyled">
+                <li>${info.telefonnummer}</li>
+                <li>${info.epost}</li>
+              </ul>
+            <p class="card-text"><small class="text-muted">${info.personligt}</small></p>
           </div>
         </div>
         `)}
@@ -60,7 +61,7 @@ class OmOss extends Base {
     </div>
 
   </div>
-</div>
+
 `;
   }
 
