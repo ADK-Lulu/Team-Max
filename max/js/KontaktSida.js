67e3e807e8e1678a6400fb3ff683441895af1187
jflo846 <floden.jennie@gmail.com>
2020-03-12 11:31:38 +0100

    fixat skalning.

--- max/js/KontaktSida.js
+++ max/js/KontaktSida.js
@@ -23,55 +23,58 @@ class KontaktSida extends Base {
 
   render() {
     return /*html*/`
-      <div class="row" route="/kontakt-sida" page-title="Kontakta oss">
-          <div class="row mx-md-n5">
-            <div class="col px-md-5">
-              <div class="p-3 border bg-light"><img src="images/misc/kontaktsida_kontorsfasad.jpg" class="card-img-top" alt="">
-              </div>
-            </div>
-            <div class="col px-md-5">
-              <div class="p-3 border bg-light">
-                <h2>Dhyr & Rumson</h2><br>Adress: Liljeholmsgatan 123, Liljeholmen<br>Öppettider:<br>Måndag-Fredag: 10-16<br>Lördag-Söndag: Stängt
-                <br>Telefon: +46 23 456 78 90<br>Email:<a href="mailto:dhyrochrumson@email.com">dhyrochrumson@email.com</a>
-              </div>
-            </div>
+      <div class="row border bg-light" route="/kontakt-sida" page-title="Kontakta oss">
+        <div class="col d-none d-lg-block">
+          <div class="p-3"><img src="images/misc/kontaktsida_kontorsfasad.jpg" class="card-img-top" alt="">
           </div>
+        </div>
+        <div class="col">
+          <div class="p-3">
+            <h2>Dhyr & Rumson</h2><br>Adress: Liljeholmsgatan 123, Liljeholmen
+            <br>Telefon: +46 23 456 78 90<br>Email: <a class="text-dark" href="mailto:dhyrochrumson@email.com">dhyrochrumson@email.com</a><br>
+            <br><p>Öppettider:<br>Måndag-Fredag: 10-16<br>Lördag-Söndag: Stängt</p>
+          </div>
+        </div>
 
-          <div class="row w-100 pt-5">
-            ${this.formSent ? /*html*/`
-              <div class="col-12">
-                <h2>
-                  <p>Tack för ditt meddelande!</p>
-                  <p>Vi återkommer till dig så snabbt som möjligt.</p>
-                </h2>
-              </div>
+        <div class="row w-100 pt-5">
+          ${this.formSent ? /*html*/`
+            <div class="col-12">
+              <h2>
+                <p>Tack för ditt meddelande!</p>
+                <p>Vi återkommer till dig så snabbt som möjligt.</p>
+              </h2>
+            </div>
+        </div>
               `:
-                /*html*/`  
-              <div class="col-5">
-                  <h4>Kontakta oss:</h4>
-              </div>
-              <div class="col-7">
-                <form submit="collectFormData">
-                    <div class="form-group">
-                      <label class="w-100">Namn:
-                        <input name="namn" type="text" class="form-control" placeholder="Ditt namn" required pattern=".{2,}">
-                      </label>
-                    </div>
-                    <div class="form-group">
-                      <label class="w-100">E-post:
-                        <input name="epost" type="email" class="form-control" placeholder="Din e-postadress" required>
-                      </label>
-                    </div>
-                    <div class="form-group">
-                      <label class="w-100">Meddelande:
-                        <textarea name="fritext" class="form-control" placeholder="Skriv ditt meddelande här" required></textarea>
-                      </label>
-                    </div>
-                    <input class="btn btn-primary float-right" type="submit" value="Skicka">
-                  </form>
-              </div> 
-            `}
+         /*html*/`  
+          <div class="col-12 col-lg-6 ml-3 text-left d-block d-lg-none">
+            <h4>Kontakta oss:</h4>
           </div>
+          <div class="col-12 col-lg-6 text-right d-none d-lg-block">
+            <h4>Kontakta oss:</h4>
+          </div>
+          <div class="col-12 col-lg-6 ml-3 ml-lg-0">
+            <form submit="collectFormData">
+              <div class="form-group">
+                <label class="w-100">Namn:
+                  <input name="namn" type="text" class="form-control" placeholder="Ditt namn" required pattern=".{2,}">
+                </label>
+              </div>
+              <div class="form-group">
+                <label class="w-100">E-post:
+                  <input name="epost" type="email" class="form-control" placeholder="Din e-postadress" required>
+                </label>
+              </div>
+              <div class="form-group">
+                <label class="w-100">Meddelande:
+                  <textarea name="fritext" class="form-control" placeholder="Skriv ditt meddelande här" required></textarea>
+                </label>
+              </div>
+                <input class="btn btn-primary float-right mb-3" type="submit" value="Skicka">
+            </form>
+          </div> 
+            `}
+        
         </div>
     `;
   }
--- max/js/OmOss.js
+++ max/js/OmOss.js
@@ -21,7 +21,7 @@ class OmOss extends Base {
   
     <!--infofält om företaget o chefen-->
     <div class="col-md-6 col-xl-6 bg-secondary">
-      <div class="card col-my-2">
+      <div class="card mt-3 col-my-2">
         <div class="card-body">
           <h4 class="card-title">Vår story</h4>
           <p class="card-text">Redan när vår grundare <strong>Fiona</strong> var en liten flicka så älskade hon hus. Hon
