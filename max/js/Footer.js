fb60c5f6b872487d29545014b2758fe133691096
jflo846 <floden.jennie@gmail.com>
2020-03-17 15:18:20 +0100

    task 4.3  ny footer

--- max/js/Footer.js
+++ max/js/Footer.js
@@ -5,34 +5,62 @@ class Footer extends Base {
 
   render() {
     return /*html*/ `
-  <footer class="bg-secondary pt-3 text-light">
-  <div class="container">
-    <div class="row align-items-end justify-content-center">
-      <div class="col-auto">
-        ${app.navBarLinks.find(x => x.label === 'Kontakt').dropdown.map(x => x.label === "Kontakta oss" ?/*html*/`
-        <a class="btn btn-info" role="button" href="${x.route}">${x.label}</a>
-        `: null)}
-      </div>
-      <div class="col-auto">
-        <div>
-          <a href="https://www.facebook.com/" target="_blank">
-            <i class="icofont-facebook icofont-2x"></i>
-          </a>
-          <a href="https://www.instagram.com/" target="_blank">
-            <i class="icofont-instagram icofont-2x"></i>
-          </a>
-        </div>
-      </div>
-    </div> 
-    <div class="row align-items-end justify-content-center">
-      <div class="col-auto">
-        <p class="mb-3 pt-3">©<small> Dhyr & Rumson 2020</small></p>
-      </div>
-    </div>
+  <footer class="footer row">
+  <div class="col-12 col-md-6 col-lg-4">
+    <h1 class="h1-responsive">Dhyr & Rumson</h1>
+    <h5>Adress</h5>
+    <p>Liljeholmsvägen 123, 12345 Liljeholmen</p>
+    <a class="btn btn-secondary mb-4" href="mailto:dhyrochrumson@email.com">Maila oss</a>
+    <p>&copy; 2020 Dhyr & Rumson All rights reserved.</p>
   </div>
   
-    
-  </footer>  
+    <div class="col-6 col-md-3">
+      <h4>Om oss</h4>
+
+      <ul class="list-group">
+        <li>
+          <a class="text-muted" href="/om-oss">Hitta mäklare</a>
+        </li>
+
+        <li>
+          <a class="text-muted" href="/kontakt-sida">Kontakta oss</a>
+        </li>
+
+        <li>
+          <a class="text-muted" href="/salj-sida">Värdera din bostad</a>
+        </li>
+      </ul>
+    </div>
+
+    <div class="col-6 col-md-3">
+      <h4>Information</h4>
+
+      <ul class="list-group">
+        <li>
+          <a class="text-muted" href="#">Användarvillkor</a>
+        </li>
+
+        <li>
+          <a class="text-muted" href="#">Personuppgiftspolicy</a>
+        </li>
+
+        <li>
+          <a class="text-muted" href="#">Sitemap</a>
+        </li>
+      </ul>
+    </div>
+
+    <div class="col-12 col-md-12 col-lg-2">
+      <h4 class="mb-3">Följ oss på:</h4>
+      <ul class="list-group">
+        <li>
+         <span class="mx-3"> <a href="http://www.facebook.com" target="_blank"><img src="/images/iconer/facebook.png"></a></span>
+          <a href="http://www.instagram.com" target="_blank"><img src="/images/iconer/instagram.png"></a>
+        </li>
+      </ul>
+    </div>
+  
+</footer>
     `;
   }
 }
