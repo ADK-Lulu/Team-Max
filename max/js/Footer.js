7d2318c577c126c4410b9c918ac45fd8e23d9036
Paatsu <johnny@wong.se>
2020-03-20 15:00:34 +0100

    Fix på footer som inte såg ok ut i desktop.

--- max/js/Footer.js
+++ max/js/Footer.js
@@ -8,26 +8,24 @@ class Footer extends Base {
   
   <footer class="footer col-12">
   <div class="row pb-5 pb-lg-1">
-  <div class="col-12 col-sm-6 col-md-6 col-lg-4">
-    <h1 class="h1-responsive">Dhyr & Rumson</h1>
-    <h5>Adress</h5>
-    <p>Liljeholmsvägen 123, 123 45 Liljeholmen</p>
-    <a class="btn btn-secondary mb-4" href="mailto:dhyrochrumson@email.com">Maila oss</a>
-    <p>&copy; 2020 Dhyr & Rumson</p>
-  </div>
+    <div class="col-12 col-sm-6 col-md-6 col-lg-4">
+      <h1 class="h1-responsive">Dhyr & Rumson</h1>
+      <h5>Adress</h5>
+      <p>Liljeholmsvägen 123, 123 45 Liljeholmen</p>
+      <a class="btn btn-secondary mb-4" href="mailto:dhyrochrumson@email.com">Maila oss</a>
+      <p>&copy; 2020 Dhyr & Rumson</p>
+    </div>
   
-    <div class="col-6 col-sm-6 col-md-6">
+    <div class="col-6 col-sm-6 col-md-6 col-lg-3">
       <h4>Om oss</h4>
 
       <ul class="list-group">
         <li>
           <a class="text-muted" href="/om-oss">Hitta mäklare</a>
         </li>
-
         <li>
           <a class="text-muted" href="/kontakt-sida">Kontakta oss</a>
         </li>
-
         <li>
           <a class="text-muted" href="/salj-sida">Värdera din bostad</a>
         </li>
@@ -36,12 +34,10 @@ class Footer extends Base {
 
     <div class="col-6 col-sm-6 col-lg-3">
       <h4>Information</h4>
-
       <ul class="list-group">
         <li>
           <a class="text-muted" href="/notFound">Användarvillkor</a>
         </li>
-
         <li>
           <a class="text-muted" href="notFound">Sitemap</a>
         </li>
