0d4bc30a8e68a601323229d9816062f8947cb17a
Paatsu <johnny@wong.se>
2020-03-21 19:17:49 +0100

    Fixat om bootstrapstrukturen på köpsidan

--- max/js/KopSida.js
+++ max/js/KopSida.js
@@ -123,170 +123,165 @@ class KopSida extends Base {
   render() {
     let s = app.settings;
     return /*html*/`
-        <div class="row" route="/kop-sida" page-title="Köpa bostad">
+      <div route="/kop-sida" page-title="Köpa bostad">
 
-          <!-- Fixed-bottom "Antal bostäder hittad för mobilvy"  -->
-          <div class="d-lg-none bg-light row shadow fixed-bottom">
-            <p class="col pt-3 ml-3">${this.results ? '<strong>' + this.results.length + '</strong> bostäder till salu just nu' : ''}</p>
-          </div>
+        <!-- Fixed-bottom "Antal bostäder hittad för mobilvy"  -->
+        <div class="row d-lg-none bg-light shadow fixed-bottom">
+          <p class="col pt-3 ml-3">${this.results ? '<strong>' + this.results.length + '</strong> bostäder till salu just nu' : ''}</p>
+        </div>
 
-          <div class="col-12">
-            <div class="row">
-              <h1 class="m-3 m-sm-0 h1-responsive py-3">Bostäder till salu i ${this.sokOrd === undefined ? "Storstockholm" : this.sokOrd}</h1>
+        <div class="row">
+          <h1 class="m-3 m-sm-0 h1-responsive py-3">Bostäder till salu i ${this.sokOrd === undefined ? "Storstockholm" : this.sokOrd}</h1>
+        </div>
+
+        <div class="row p-3 mb-4">${this.sokning}</div>
+
+        <!-- Filterknappar-->
+        <form class="row bg-secondary no-gutters rounded mb-4 align-items-center">
+          <div class="col-3 text-center checkmark">
+            <div class="btn px-0 py-md-3 pl-md-2 pr-md-4 mt-md-2">
+              <input change="checkBoxFilter" value="false" type="checkbox" id="sokBostadsratt" ${app.settings.sokBostadsratt ? 'checked' : ''}>
+              <label for="sokBostadsratt" class="rounded">              
+                <img class="icon-filter" src="/images/iconer/bostadsratt.svg" alt="bostadsratt">
+                <div class="label font-weight-bold">Bostadsrätt</div>
+                <span class="checkmarking"></span>
+              </label>
             </div>
-        
-            <div class="row p-3 mb-4">${this.sokning}</div>
-    
-              <!-- Filterknappar-->
-            	<form class="row bg-secondary rounded mb-4">
-                <div class="form-row col-12 align-items-center">
-                  <div class="col-3 text-center checkmark">
-                    <div class="btn px-0 py-md-3 pl-md-3 pr-md-4 mt-md-2">
-                      <input change="checkBoxFilter" value="false" type="checkbox" id="sokBostadsratt" ${app.settings.sokBostadsratt ? 'checked' : ''}>
-                      <label for="sokBostadsratt" class="rounded">               
-                        <img class="icon-filter" src="/images/iconer/bostadsratt.svg" alt="bostadsratt">
-                        <div class="label font-weight-bold">Bostadsrätt</div>
-                        <span class="checkmarking"></span>
-                      </label>
-                    </div>
-                  </div>
-                  <div class="col-3 text-center checkmark">
-                    <div class="btn px-0 py-md-3 pl-md-3 pr-md-4 mt-md-2">
-                      <input change="checkBoxFilter" value="true" type="checkbox" id="sokRadhus" ${app.settings.sokRadhus ? 'checked' : ''}>
-                      <label for="sokRadhus" class="rounded"> 
-                        <img class="icon-filter" src="/images/iconer/radhus.svg" alt="radhus">
-                        <div class="label font-weight-bold">Radhus</div>
-                        <span class="checkmarking"></span>
-                      </label>
-                    </div>
-                  </div>
-                  <div class="col-3 text-center checkmark">
-                    <div class="btn px-0 py-md-3 pl-md-3 pr-md-4 mt-md-2">
-                      <input change="checkBoxFilter" value="true" type="checkbox" id="sokVilla" ${app.settings.sokVilla ? 'checked' : ''}>
-                      <label for="sokVilla" class="rounded"> 
-                        <img class="icon-filter" src="/images/iconer/villa.svg" alt="villa">
-                        <div class="label font-weight-bold">Villa</div>
-                        <span class="checkmarking"></span>
-                      </label>
-                    </div>
-                  </div>
-                  <div class="col-3 text-center checkmark">
-                    <div class="btn px-0 py-md-3 pl-md-3 pr-md-4 mt-md-2">
-                      <input change="checkBoxFilter" value="false" type="checkbox" id="sokNybygge" ${app.settings.sokNybygge ? 'checked' : ''}>
-                      <label for="sokNybygge" class="rounded"> 
-                        <img class="icon-filter" src="/images/iconer/nyproduktion.svg" alt="nyproduktion">
-                        <div class="label font-weight-bold" font-size="1vw">Endast Nyproduktion</div>
-                        <span class="checkmarking checkmarking-nyproduktion"></span>
-                      </label>
-                    </div>
-                  </div>
-                </div>
-              </form>
+          </div>
+          <div class="col-3 text-center checkmark">
+            <div class="btn px-0 py-md-3 pl-md-2 pr-md-4 mt-md-2">
+              <input change="checkBoxFilter" value="true" type="checkbox" id="sokRadhus" ${app.settings.sokRadhus ? 'checked' : ''}>
+              <label for="sokRadhus" class="rounded"> 
+                <img class="icon-filter" src="/images/iconer/radhus.svg" alt="radhus">
+                <div class="label font-weight-bold">Radhus</div>
+                <span class="checkmarking"></span>
+              </label>
+            </div>
+          </div>
+          <div class="col-3 text-center checkmark">
+            <div class="btn px-0 py-md-3 pl-md-2 pr-md-4 mt-md-2">
+              <input change="checkBoxFilter" value="true" type="checkbox" id="sokVilla" ${app.settings.sokVilla ? 'checked' : ''}>
+              <label for="sokVilla" class="rounded"> 
+                <img class="icon-filter" src="/images/iconer/villa.svg" alt="villa">
+                <div class="label font-weight-bold">Villa</div>
+                <span class="checkmarking"></span>
+                </label>
+            </div>
+          </div>
+          <div class="col-3 text-center checkmark">
+            <div class="btn px-0 py-md-3 pl-md-2 pr-md-4 mt-md-2">
+              <input change="checkBoxFilter" value="false" type="checkbox" id="sokNybygge" ${app.settings.sokNybygge ? 'checked' : ''}>
+              <label for="sokNybygge" class="rounded"> 
+                <img class="icon-filter" src="/images/iconer/nyproduktion.svg" alt="nyproduktion">
+                <div class="label font-weight-bold" font-size="1vw">Endast Nyproduktion</div>
+                <span class="checkmarking checkmarking-nyproduktion"></span>
+              </label>
+            </div>
+          </div>
+        </form>
 
-                <!-- Input range sliders -->
-                ${!this.visaFilter ? '' : /*html*/`
-                  <div class="row p-0">
-                    <div class="col-12 col-md-4 pb-2 mb-2">
-                      <label class="w-100">Minst antal rum: ${s.minRum}                   
-                        <input value="${s.minRum}" type="range" class="form-control-range" min="0" max="5" step="1" id="minRum" input="getSliderValue">
-                      </label>
-                      <div class="w-100"></div>
-                      <label class="w-100">Max. antal rum: ${s.maxRum > 5 ? '5+' : s.maxRum}
-                        <input value="${s.maxRum}" type="range" class="form-control-range" min="0" max="5" step="1" id="maxRum" input="getSliderValue">
-                      </label>
-                    </div>
-                    <div class="col-12 col-md-4 pb-2 mb-2">
-                      <label class="w-100">Minst boarea: ${(s.minKvm * 10) > 300 ? '300+' : (s.minKvm * 10)} kvm
-                        <input value="${s.minKvm}" type="range" class="form-control-range" min="0" max="30" step="1" id="minKvm" input="getSliderValue">
-                      </label>
-                      <div class="w-100"></div>
-                      <label class="w-100">Max. boarea: ${(s.maxKvm * 10) > 300 ? '300+' : (s.maxKvm * 10)} kvm
-                        <input value="${s.maxKvm}" type="range" class="form-control-range" min="0" max="30" step="1" id="maxKvm" input="getSliderValue">
-                      </label>
-                    </div>
-                    <div class="col-12 col-md-4 pb-2 mb-2">
-                      <label class="w-100">Minsta pris: ${app.formateraPris((s.minPris * 100000) > 9000000 ? '9 000 000+' : (s.minPris * 100000))} kr
-                        <input value="${s.minPris}" type="range" class="form-control-range" min="0" max="90" step="1" id="minPris" input="getSliderValue">
-                      </label>
-                      <div class="w-100"></div>
-                      <label class="w-100">Max pris: ${app.formateraPris((s.maxPris * 100000) > 9000000 ? '9 000 000+' : (s.maxPris * 100000))} kr
-                        <input value="${s.maxPris}" type="range" class="form-control-range" min="0" max="90" step="1" id="maxPris" input="getSliderValue">
-                      </label>
-                    </div>
-                  </div>
-                `}
+        <!-- Input range sliders -->
+        ${!this.visaFilter ? '' : /*html*/`
+          <div class="row p-0">
+            <div class="col-12 col-md-4 pb-2 mb-2">
+              <label class="w-100">Minst antal rum: ${s.minRum}                   
+                <input value="${s.minRum}" type="range" class="form-control-range" min="0" max="5" step="1" id="minRum" input="getSliderValue">
+              </label>
+              <div class="w-100"></div>
+              <label class="w-100">Max. antal rum: ${s.maxRum > 5 ? '5+' : s.maxRum}
+                <input value="${s.maxRum}" type="range" class="form-control-range" min="0" max="5" step="1" id="maxRum" input="getSliderValue">
+              </label>
+            </div>
+            <div class="col-12 col-md-4 pb-2 mb-2">
+              <label class="w-100">Minst boarea: ${(s.minKvm * 10) > 300 ? '300+' : (s.minKvm * 10)} kvm
+                <input value="${s.minKvm}" type="range" class="form-control-range" min="0" max="30" step="1" id="minKvm" input="getSliderValue">
+              </label>
+              <div class="w-100"></div>
+              <label class="w-100">Max. boarea: ${(s.maxKvm * 10) > 300 ? '300+' : (s.maxKvm * 10)} kvm
+                <input value="${s.maxKvm}" type="range" class="form-control-range" min="0" max="30" step="1" id="maxKvm" input="getSliderValue">
+              </label>
+            </div>
+            <div class="col-12 col-md-4 pb-2 mb-2">
+              <label class="w-100">Minsta pris: ${app.formateraPris((s.minPris * 100000) > 9000000 ? '9 000 000+' : (s.minPris * 100000))} kr
+                <input value="${s.minPris}" type="range" class="form-control-range" min="0" max="90" step="1" id="minPris" input="getSliderValue">
+              </label>
+              <div class="w-100"></div>
+              <label class="w-100">Max pris: ${app.formateraPris((s.maxPris * 100000) > 9000000 ? '9 000 000+' : (s.maxPris * 100000))} kr
+                <input value="${s.maxPris}" type="range" class="form-control-range" min="0" max="90" step="1" id="maxPris" input="getSliderValue">
+              </label>
+            </div>
+          </div>
+        `}
 
-              <!-- Sortering -->
-              <div class="row">
-                <div class="form-group col-4 mb-4">
-                <label for="sort-by">${this.results ? '(' + this.results.length + ')' : ''} Sortera efter</label>
-                  <select class="form-control" id="sort-by" click="sortera">
-                    <option value="nyast">Nyast först</option>
-                    <option value="aldst">Äldst först</option>
-                    <option value="billigastPris">Billigast först</option>
-                    <option value="dyrastPris">Dyrast först</option>
-                  </select>
-                </div>
-              </div>
-
-              <!-- Knapp för att visa/dölja filter -->
-              <div class="row">
-                <div class="col-12">
-                  <div click="visaDoljFilter" class="sort-trigger">
-                    <i class="icofont-arrow-${!this.visaFilter ? 'down' : 'up'}"></i>
-                    <span>${!this.visaFilter ? 'Fler filter' : 'Dölj filter'}</span>
-                  </div>
-                </div>
-              </div>
-
-              <!-- Sökresultat -->
-              ${this.results && this.results.length > 0 ? this.results.map(object => /*html*/`
-                <a class="text-dark" href="/objekt-sida/${object.objektId}">
-                  <div class="row mb-4 bg-grey">
-                    <div class="px-0 pr-md-0 p-xs-0 col-12 col-lg-8 col-xl-9">
-                      <!-- Nyproduktions-badge -->
-                      ${object.nyproduktion ? /*html*/`
-                        <div class="position-absolute float-left badge badge-secondary m-2">
-                          <h3 class="text-light pt-1 px-1">Nyproduktion</h3>
-                        </div>
-                      ` : ''}
-                      <!--"infoclip" "Visning idag" ifall dagens datum stämmer med visningsdatumet i db -->
-                      ${object.visning === this.todayDate.toISOString().split("").slice(0, 10).join("") ? /*html*/`
-                      <div>
-                        <span class="onsale-section text-dark pt-1 px-1"><span class="onsale">Visning idag</span></span>
-                      </div>
-                      ` : ''}
-                      <img class="img-fluid crop-image mb-0" src="${object.bildUrl}" alt="Husets bild objektnummer: ${object.objektId}">
-                    </div>
-                    <div class="py-sm-3 col-12 col-lg-4 col-xl-3">
-                      <div class="row p-2 mt-1 p-md-1 pl-md-0">
-                        <div class="col-6 col-lg-12">
-                          <h2 class="sokresultat-title text-break">${object.gata} ${object.gatunummer}</h2>
-                          <h3 class="sokresultat-title">${object.namn}</h3>
-                        </div>
-                        <div class="col-6 col-lg-12">
-                          <p class="mb-1"><span class="font-weight-bold">Boarea:</span> ${object.kvm} kvm</p>
-                          <p class="mb-1"><span class="font-weight-bold">Pris:</span> ${app.formateraPris(object.pris)} kr</p>
-                          <p class="mb-1"><span class="font-weight-bold">Rum:</span> ${object.antalRum}</p>
-                          <p class="mb-1"><span class="font-weight-bold">Typ:</span> ${object.typNamn}</p>
-                          <p class="mb-1"><span class="font-weight-bold">Visning:</span> ${object.visning}</p>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                </a>
-              `) : /*html*/`
-              <div class="row">
-                <div class="col-12">
-                  <h3 class="text-center text-dark py-2 px-3">Tyvärr matchar din sökning inget av våra objekt, gör om din sökning
-                    eller kontakta en
-                    av våra mäklare för information om kommande försäljningar.</h3>
-                </div>
-              </div> 
-            `}
+        <!-- Sortering -->
+        <div class="row">
+          <div class="form-group col-5 col-md-4 mb-4">
+          <label for="sort-by">${this.results ? '(' + this.results.length + ')' : ''} Sortera efter</label>
+            <select class="form-control" id="sort-by" click="sortera">
+              <option value="nyast">Nyast först</option>
+              <option value="aldst">Äldst först</option>
+              <option value="billigastPris">Billigast först</option>
+              <option value="dyrastPris">Dyrast först</option>
+            </select>
           </div>
         </div>
-              `;
-  }
+
+        <!-- Knapp för att visa/dölja filter -->
+        <div class="row">
+          <div class="col-12">
+            <div click="visaDoljFilter" class="sort-trigger">
+              <i class="icofont-arrow-${!this.visaFilter ? 'down' : 'up'}"></i>
+              <span>${!this.visaFilter ? 'Fler filter' : 'Dölj filter'}</span>
+            </div>
+          </div>
+        </div>
+
+        <!-- Sökresultat -->
+        ${this.results && this.results.length > 0 ? this.results.map(object => /*html*/`
+          <a class="text-dark" href="/objekt-sida/${object.objektId}">
+            <div class="row no-gutters mb-4 bg-grey">
+              <div class="px-0 pr-md-0 p-xs-0 col-12 col-lg-8 col-xl-9">
+                <!-- Nyproduktions-badge -->
+                ${object.nyproduktion ? /*html*/`
+                  <div class="position-absolute float-left badge badge-secondary m-2">
+                    <h3 class="text-light pt-1 px-1">Nyproduktion</h3>
+                  </div>
+                ` : ''}
+                <!--"infoclip" "Visning idag" ifall dagens datum stämmer med visningsdatumet i db -->
+                ${object.visning === this.todayDate.toISOString().split("").slice(0, 10).join("") ? /*html*/`
+                <div>
+                  <span class="onsale-section text-dark pt-1 px-1"><span class="onsale">Visning idag</span></span>
+                </div>
+                ` : ''}
+                <img class="img-fluid crop-image mb-0" src="${object.bildUrl}" alt="Husets bild objektnummer: ${object.objektId}">
+              </div>
+              <div class="py-sm-3 col-12 col-lg-4 col-xl-3">
+                <div class="row p-2 mt-1 p-md-1 pl-md-0">
+                  <div class="col-6 col-lg-12">
+                    <h2 class="sokresultat-title text-break">${object.gata} ${object.gatunummer}</h2>
+                    <h3 class="sokresultat-title">${object.namn}</h3>
+                  </div>
+                  <div class="col-6 col-lg-12">
+                    <p class="mb-1"><span class="font-weight-bold">Boarea:</span> ${object.kvm} kvm</p>
+                    <p class="mb-1"><span class="font-weight-bold">Pris:</span> ${app.formateraPris(object.pris)} kr</p>
+                    <p class="mb-1"><span class="font-weight-bold">Rum:</span> ${object.antalRum}</p>
+                    <p class="mb-1"><span class="font-weight-bold">Typ:</span> ${object.typNamn}</p>
+                    <p class="mb-1"><span class="font-weight-bold">Visning:</span> ${object.visning}</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </a>
+          `) : /*html*/`
+          <div class="row">
+            <div class="col-12">
+              <h3 class="text-center text-dark py-2 px-3">Tyvärr matchar din sökning inget av våra objekt, gör om din sökning
+                eller kontakta en
+                av våra mäklare för information om kommande försäljningar.</h3>
+            </div>
+          </div> 
+        `}
+      </div>
+  `}
 
 }
