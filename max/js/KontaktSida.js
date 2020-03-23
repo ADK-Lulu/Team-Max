bcc6780e52e99639572e5d4f32f4f1c977d3bf18
teknikMarit <teknikMarit@gmail.com>
2020-03-23 14:08:27 +0100

    5.4 padding o resp kontaktsida

--- max/js/KontaktSida.js
+++ max/js/KontaktSida.js
@@ -24,36 +24,32 @@ class KontaktSida extends Base {
   render() {
     return /*html*/`
       <div route="/kontakt-sida" page-title="Kontakta oss">
-        <div class="row m-3 m-sm-0">
-          <div class="col-sm-6 d-none d-md-block">
-            <div class="mt-3">
-              <img src="images/misc/office.jpg" class="img-fluid crop-image">
-            </div>
+        <div class="row">
+          <div class="col-sm-6 d-none d-md-block my-4">
+            <img src="images/misc/office.jpg" class="img-fluid crop-image">
           </div>
-          <div class="col-md-5">
-            <div class="mt-3">
+          <div class="col-md-6 mt-3">
               <img src="images/misc/logga_DR.png" class="img-fluid">
               <br>Adress: Liljeholmsgatan 123, Liljeholmen
               <br>Telefon: +46 23 456 78 90<br>Email: <a class="text-dark" href="mailto:dhyrochrumson@email.com">dhyrochrumson@email.com</a><br>
               <br><p>Öppettider:<br>Måndag-Fredag: 10-16<br>Lördag-Söndag: Stängt</p>
-            </div>
           </div>
         </div>
 
-        <div class="row pt-3 px-4 py-3">
+        <div class="row pt-3 px-2 py-3">
           ${this.formSent ? /*html*/`
             <div class="col-12">
-              <h2 class="text-center text-dark py-5 px-3">
+              <h1 class="h1-responsive text-center text-dark py-5 px-3">
                 Tack för ditt meddelande!<br>
                 Vi återkommer till dig så snabbt som möjligt.
-              </h2>
+              </h1>
             </div>
               `:
          /*html*/`  
-         <div class="col-4 d-none d-sm-block text-right bg-secondary pt-3 mb-4">
-            <h4 class="h1-responsive ml-3">Kontakta oss:</h4>
+         <div class="col-4 col-md-6 text-right bg-secondary pt-3 mb-4">
+            <h1 class="h1-responsive">Kontakta oss:</h1>
           </div>
-          <div class="col-sm-8 bg-secondary px-4 pt-3 mb-4">
+          <div class="col-8 col-md-6 bg-secondary pt-3 mb-4 ">
             <form submit="collectFormData">
               <div class="form-group">
                 <label class="w-100">Namn:
