9b7320da8782f18ce9f0ba546001b63e1b2cdec0
ADK-Lulu <ulrika.alehag@gmail.com>
2020-03-26 10:36:25 +0100

    Städade på objektsidan

--- max/js/ObjektSida.js
+++ max/js/ObjektSida.js
@@ -8,13 +8,11 @@ class ObjektSida extends Base {
   }
 
   closeModal() {
-    // Needed to stop scrolling when the modal is shown
     this.shown = false;
     setTimeout(() => $('body').removeClass('modal-open'), 0);
     this.render();
   }
 
-  // Använd databasen max
   async mount() {
 
     // Hämta alla bilder för aktuell SaljObjekt
@@ -58,17 +56,11 @@ class ObjektSida extends Base {
         objektSidaId: this.objektId
       });
 
-    //Hämta detaljer
-
-    //Hitta bilden som är framsidebild för aktuell SaljObjekt
+    //Hämta diverse-detaljer(datum, bilder, mäklare)
     this.frontImage = this.images.find(front => front.framsidebild);
-    //Hitta bilden för planritning
     this.planImage = this.images.find(plan => plan.planritning);
-    //Hitta alla övriga bilder
     this.allPictures = this.images.filter(pictures => !pictures.framsidebild && !pictures.planritning);
-    //Plocka ut mäklarobjektet
     this.maklare = this.maklarinfo[0];
-    //Dagens datum - att använda till "visning idag"
     this.todayDate = new Date();
 
 
@@ -97,7 +89,6 @@ class ObjektSida extends Base {
       formData[element.name] = element.value;
 
     }
-    // Sänder till db
     await sql(/*sql*/`
       INSERT INTO KopKontakter(namn, epost) VALUES($namn, $epost)
     `, formData);
@@ -106,18 +97,17 @@ class ObjektSida extends Base {
     this.render();
   }
 
-  kopieraLank() {
-    /* Get the text field */
+  kopieraLank() {//Kod för dela-länken
+    //Hitta text-fältet
     let copyText = document.getElementById("lank")
     console.log(copyText)
 
-    /* Select the text field */
+    //Välj text fältet
     copyText.select();
-    copyText.setSelectionRange(0, 99999); /*For mobile devices*/
+    copyText.setSelectionRange(0, 99999); /*För mindre skärmar*/
 
-    /* Copy the text inside the text field */
+    //kopiera texten i text-fältet
     document.execCommand("copy");
-
     document.getElementById("kopieratLank").innerHTML = "Länk kopierad till clipboard!";
   }
 
@@ -126,14 +116,11 @@ class ObjektSida extends Base {
     this.render();
   }
 
-
-
-
   render() {
     return /*html*/`
       <div route="/objekt-sida/${this.objektId}" page-title="Visa objekt ${this.objektId}">
         <div class="row">
-          <div class="col-12">
+          <div class="col-12 mt-4 px-0">
             <img src="${this.frontImage.bildUrl}" class="img-fluid crop-image"  alt="Frontbild ${this.objektId}">
             <!--Badge "Visning idag" ifall dagens datum stämmer med visningsdatumet i db -->
             ${this.visning === this.todayDate.toISOString().split("").slice(0, 10).join("") ? /*html*/`
@@ -143,19 +130,20 @@ class ObjektSida extends Base {
             ` : ''}
           </div>
         </div>
-        <div class="row ml-3">
-         <button type="button" click="favorit" class="btn btn-secondary white-btn-text">${store.favoriter.includes(this.objektId) ?  /*html*/`<i class="icofont-heart card-title-gold icon-text"></i> Ta bort från favoriter`
+        <div class="row">
+         <button type="button" click="favorit" class="btn btn-secondary white-btn-text">${store.favoriter.includes(this.objektId) ?  /*html*/`
+         <i class="icofont-heart card-title-gold icon-text"></i> Ta bort från favoriter`
         :/*html*/`<i class="icofont-heart text-white icon-text"></i> Spara som favorit`}
          </button>
         </div>
         <div class="row">
           <div class="col-12"> 
-            <h1 class="text-center m-3 h1-responsive">${this.saljRubrik}</h1> 
+            <h1 class="text-center m-2 h1-responsive">${this.saljRubrik}</h1> 
           </div>
         </div>
         <div class="row sticky-top" id="stickyTop">
           <div class="col-12 text-center"><!--Kod för att knapparna ska vara centrerade och sticky när man scrollar.-->
-            <div class="btn-group btn-group-lg text-center tab-choices-object-page" role="group" aria-label="Basic-example"> <!--Kod för button group -->
+            <div class="btn-group btn-group-lg text-center tab-choices-object-page" role="group" aria-label="Basic-example">
               <div class="row">
                 <div class="btn-group col-12 col-lg-6">
                   <button type="button" class="btn btn-secondary py-3 white-btn-text" onclick="window.location.href='#AllaBilder'">
@@ -183,9 +171,6 @@ class ObjektSida extends Base {
             </div>
           </div>
         </div>
-    <!--Skriv kod här som inte har med knapparna att göra-->
-        
-          <!--Hårdfakta-ruta här-->
         <div class="row no-gutters mt-4 pt-3 px-3 pb-lg-3 bg-light">
           <div class="col-12 col-md-6 col-lg-4">
             <div class="pb-2"><span class="font-weight-bold"><img src="/images/iconer/money.png"> Pris: </span>${app.formateraPris(this.pris)} kr</div>
@@ -197,18 +182,15 @@ class ObjektSida extends Base {
           <div class="col-12 col-md-6 col-lg-4">
             <div class="pb-2"><span class="font-weight-bold"><img src="/images/iconer/location.png"> Område: </span> ${this.namn}</div>
             <div class="pb-2"><span class="font-weight-bold"><img src="/images/iconer/spade.png"> Byggnadsår:  </span>${this.byggAr}</div>
-            ${this.typId === 2 || this.typId === 3 ? /*html*/`<div class="pb-2"><span class="font-weight-bold"><img src="/images/iconer/drift.png">  Avgift: </span>${this.avgift} kr/mån</div>`
-        :/*html*/`<div class="pb-2"><span class="font-weight-bold"><img src="/images/iconer/drift.png">  Driftkostnad: </span>${this.driftKostnad} kr/år</div>`}
+            ${this.typId === 2 || this.typId === 3 ? /*html*/`<div class="pb-2"><span class="font-weight-bold"><img src="/images/iconer/drift.png">  Avgift: </span>${app.formateraPris(this.avgift)} kr/mån</div>`
+        :/*html*/`<div class="pb-2"><span class="font-weight-bold"><img src="/images/iconer/drift.png">  Driftkostnad: </span>${app.formateraPris(this.driftKostnad)} kr/år</div>`}
             <div class="pb-2"><span class="font-weight-bold"><img src="/images/iconer/car.png"> Garage: </span>${this.garage ? 'finns' : 'finns ej'}</div>
             <div class="pb-2"><span class="font-weight-bold"> <img src="/images/iconer/hiss.png"> Hiss: </span>${this.hiss ? 'finns' : 'finns ej'}</div>
           </div>
-           <!--Kod för säljtext-->
           <div class="col-12 col-lg-4 pt-3 pt-lg-0">
             <p class="text-left text-break">${this.saljText}</p>
           </div>
         </div>
-      
-        <!--Kod för bilderna-->
         <div class="row"><a id="AllaBilder"></a>
           <h3 class="col-12 pt-5 py-2">Alla bilder</h3>
             ${this.allPictures.map(image => /*html*/`
@@ -217,14 +199,12 @@ class ObjektSida extends Base {
             </div>
           `)}
         </div>
-        <!--Kod för planritning-->
         <div class="row"><a id="Planritning"></a>
           <h3 class="col-12 pt-5 py-2">Planritning</h3>
             <div class="col-12 col-med-6">
               <img src="${this.planImage.bildUrl}" class="img-fluid py-2" alt="Planritning ${this.objektId}">
             </div>
         </div>
-        <!--Fakta om-->
         <div class="row" id="FaktaOm">
           <h3 class="col-12 text-align-center pt-5 py-2">Fakta om</h3>
           <div class="col-12 col-lg-6">
@@ -234,7 +214,6 @@ class ObjektSida extends Base {
             <p class="text-break">${this.objektBeskrivning2}</p>
           </div>
         </div>     
-        <!--Aktuell mäklare för objektet presenteras, Marit parprogr med Sören (i något skede)-->
         <div class="row align-items-center py-5" id="AnmälIntresse">
           <h3 class="col-12 text-align-center pt-5 py-2">Anmäl intresse</h3>
             <div class="col-6 col-md-3">
@@ -248,7 +227,7 @@ class ObjektSida extends Base {
               <li class="list-group-item text-break">${this.maklare.epost}</li>
             </ul>
           </div>        
-          <!--Tack för visat intresse (svar på intresseformulär)-->
+          <!--Intresseformulär-->
             ${this.formSent ? /*html*/`
             <div class="col-6">
               <h2 class="text-center text-dark py-5 px-3">
@@ -257,7 +236,6 @@ class ObjektSida extends Base {
               </h2>
             </div>
              ` :
-            //Intresseformulär
           /*html*/`
           <div class="col-12 col-md-6 mt-3 mt-md-0">
            <div class="col mt-2 pr-0">
@@ -270,7 +248,7 @@ class ObjektSida extends Base {
                 </div>
                 <div class="form-group">
                   <label class="w-100">E-post:
-                   <input name="epost" type="email" class="form-control" autocomplete="email" placeholder="namn@mail.com" required>
+                   <input name="epost" type="email" class="form-control" autocomplete="email" placeholder="namn@email.com" required>
                   </label>
                  </div>
                  <input class="btn btn-primary float-right" type="submit" value="Skicka">
@@ -278,8 +256,9 @@ class ObjektSida extends Base {
              </div>
              </div>
            `}
-          </div><!--mäklarinfo slut-->
-          <div class="row align-items-end" id="OmOmrådet"><!--Om området-->
+          </div>
+          <!--mäklarinfo slut-->
+          <div class="row align-items-end" id="OmOmrådet">
            <div class="col-12 col-lg-4 mt-4">
             <h3>Om ${this.namn}:</h3>
              ${this.omradesBeskrivning}
@@ -288,7 +267,6 @@ class ObjektSida extends Base {
             <img class="crop-image" src="${this.bildUrl}">
            </div>
           </div>
-          <!--Koden för dela-knappen-->
           <div id="Dela">
             <div class="modal-backdrop ${this.shown ? 'show' : 'd-none'}"></div>
               <div class="modal ${this.shown ? 'd-block open' : ''}" tabindex="-1" role="dialog">
