6407d6c394903915eded235bd237d0f524851f06
jflo846 <floden.jennie@gmail.com>
2020-03-26 11:37:01 +0100

    gjort om om-oss sidan.

--- max/js/OmOss.js
+++ max/js/OmOss.js
@@ -22,33 +22,23 @@ class OmOss extends Base {
   render() {
     return /*html*/`
 <div class="row" route="/om-oss" page-title="Om oss">
-  
-  <!--infofält om företaget o chefen-->
-  <div class="col-md-6 col-xl-6 bg-gold my-4">
-    <div class="card my-1 col-my-1">
-      <div class="card-body">
-        <h4 class="card-title">Vår story</h4>
-        <p class="card-text">Redan när vår grundare <strong>Fiona</strong> var en liten flicka så älskade hon hus. Hon ritade hus.
-          Hon byggde hus av sockerbitar. Hon byggde hus av lego. Hon byggde pepparkakshus som hon sedan sprängde med smällare.
-          Och nu får hon jobba med det hon älskar mest: Att ta dina pengar för att köpa hus. Eller med hennes egna ord: 
-        </p>
-        <p class="card-text"><small class="text-muted">
-          <h4><i>"Dina besparingar - mitt semesterparadis på Rhodos."</i></h4></small>
-        </p>
-        <img class="card-img-bottom" src="/images/maklare/2.jpg" alt="Företagschefen">
-      </div>
+  <div class="row">
+    <div class="col-12">
+      <h1 class="h1-responsive pl-3 pl-sm-0 pr-3 mt-3">Vi är Dhyr & Rumson</h1>
     </div>
   </div>
 
   <!--info om samt bilder från db om mäklarna.-->
-  <div class="col-md-6 col-xl-6 my-4">
-    <div class="row card-columns">
+  <div class="col-12 my-4">
+    <div class="row">
       ${this.maklarInfo.map(info =>/*html*/`
-        <div class="col-6 col-md-6 card-mb-2">
+        <div class="col-12 col-md-4 pl-sm-5 card-mb-2">
           <img class="card-img-top" src="${info.bildUrl}" alt="Card image cap">
-          <div class="card-body text-center">
+          <div class="card-body text-center" >
             <h5 class="card-title-gold">${info.namn}</h5>
-            <p class="card-text text-muted">"${info.personligt}"</p>
+            <p class="card-text"><small class="text-muted">
+              <h4 class="blockquote"><i>"${info.personligt}"</i></h4></small>
+            </p>
             <ul class="list-unstyled">
               <li class="text-break mt-2"><a class="text-dark" href="mailto:${info.epost}">${info.epost}</a></li>
               <li>${info.telefonnummer}</li>
@@ -56,9 +46,29 @@ class OmOss extends Base {
           </div>
         </div>
       `)}
+    
+  
+      <!--infofält om företaget o chefen-->
+      <div class="justify-content-end">
+        <div class="col-12 mt-5 my-1 col-my-1">
+          <div class="card-style">
+            <div class="card-body border border-warning">
+              <h1 class="h1-responsive pl-3 pl-sm-0 pr-3">Vår story</h1>
+              <p class="card-text">Redan när vår grundare <strong>Fiona</strong> var en liten flicka så älskade hon hus.
+              Hon byggde pepparkakshus som hon sedan sålde till högstbjudande.<br>
+              Nu får hon jobba med det hon älskar mest.<br> Tillsammans med sina fantastiska medarbetare omvandlar hon hus till pengar.<br> 
+              Eller med hennes egna ord: 
+              </p>
+              <p class="card-text"><small class="text-muted">
+                <h4 class="blockquote"><i>"Dina besparingar - mitt semesterparadis på Rhodos."</i></h4></small>
+              </p>
+            </div>  
+          </div>
+        </div>
+      </div>
+
     </div>
   </div>
-
 </div>
 
 `;
